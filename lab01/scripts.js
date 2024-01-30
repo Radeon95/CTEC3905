b28d5afd96129b50db6e95765e91d3f95fe4e45f
P2646750 <P2646750@my365.dmu.ac.uk>
2024-01-30 21:42:16 +0000

    "lab-01: a basic HTML template with some styles and JavaScript"

---
+++ lab01/scripts.js
@@ -0,0 +1,5 @@
+// alert("Hello World !");
+console.log("Hello WOrld!");
+// document.querySelector(".first").textContent = "I've changed the content!";
+// const first = document.getElementById("first");
+// first.textContent = "I've changed the content!";
